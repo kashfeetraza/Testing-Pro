03b9ea0778bc218205a9e4177a867d4e1b9b22b9
Kashfeet Raza <kashfeetraza@gmail.com>
2025-11-01 23:20:08 +0500

    Add files via upload

---
+++ All programs.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	                 //Program of swapping
+	//string name;
+	//int num1=30;
+	//int num2=20;
+	//int temp;
+	//cout<<"enter name=";
+	//getline(cin,name);
+	//cout<<name;
+	//cout<< "the number before sawaing="<< num1 << "\t"<< num2<<endl;
+	//temp=num1;
+	//num1=num2;
+	//num2=temp;
+	//cout<<"----------------------------------------------"<< endl;
+	//cout<< "the number after swaping="<< num1<<"\t"<< num2<<endl;
+	
+	                //Program of enter and knowing small letter and capital letters
+	//char smallchar;
+	//char capitalchar;
+	//cout <<"Enter capital letter char="<<endl;
+	//cin>>capitalchar;
+	//	cout<<"capital letter is ="<<capitalchar<< endl;
+	// smallchar=capitalchar+32;
+	// cout<<"the capital letter of the small letter is="<<smallchar;
+	
+	              // Program of marks and using if else
+	//int mark;
+	//cout<< "Enter the Number="<< endl;
+	//cin>> mark;
+	//if(mark>=50)
+	//{
+	//cout<<"A";
+	//}
+	//else{
+	//cout<<"you are fail";
+	//}
+	
+	                 //Program of bill generate
+	//const float tax=0.10;
+    //float p1,p2,p3,p4,p5;
+    //float total;
+    //float sub_total;
+    //float sale_tax;
+    //float totalbill;
+    //cout<<"Enter First item="<< endl;
+    //cin>>p1;
+    //cout<<"--------------------------"<<endl;
+    //cout<<"Enter Second item="<< endl;
+    //cin>>p2;
+    //cout<<"--------------------------"<<endl;
+    //cout<<"Enter Third item="<< endl;
+    //cin>>p3;
+    //cout<<"--------------------------"<<endl;
+    //cout<<"Enter Fourth item="<< endl;
+    //cin>>p4;
+    //cout<<"--------------------------"<<endl;
+    //cout<<"Enter Fifth item="<< endl;
+    //cin>>p5;
+    //cout<<"--------------------------"<<endl;
+    //sub_total=p1+p2+p3+p4+p5;
+    //cout<<"Total subtotal="<< sub_total<< endl;
+    //cout<<"--------------------------"<<endl;
+    //sale_tax=sub_total*tax;
+    //cout<<"sale tax="<< sale_tax<< endl;
+    //cout<<"--------------------------"<<endl;
+    //totalbill=sub_total+sale_tax;
+    //cout<<"Total bill="<<totalbill<<endl;
+    //cout<<"--------------------------"<<endl;
+	
+	return 0;
+}
