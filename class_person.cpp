c42b5221286d7d458d17e53ce3c54fd058d385ab
Kashfeet Raza <kashfeetraza@gmail.com>
2025-11-02 20:10:41 +0500

    Add files via upload

---
+++ class_person.cpp
@@ -0,0 +1,13 @@
+class Person{
+string name;
+int age;
+public:
+	Person(string name, int age)(
+	 this->name=name;
+	 this->age=age;
+		 )
+	 void display(){
+	cout<<"am"<<name<<"and i am"<<age<<"years old"<<endl;
+	 }
+ };
+
