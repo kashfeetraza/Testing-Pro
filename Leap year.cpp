6bfc374d1ddc190df1f6411cbe604b45a919105a
Kashfeet Raza <kashfeetraza@gmail.com>
2025-11-04 19:50:59 +0500

    Add files via upload

---
+++ Leap year.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int year;
+	cout<<"Enter to check leap year:"<<endl;
+	cin>>year;
+	
+	if(year%4==0){
+	cout<<"Its leap year"<<endl;
+	
+	}else
+	cout<<"its not leap year"<<endl;
+	
+	return 0;
+ 
+}
