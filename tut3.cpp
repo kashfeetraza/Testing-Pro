c70454b36a96d10a9b3008c825d719c2f984b87a
Kashfeet Raza <kashfeetraza@gmail.com>
2025-11-05 21:50:08 +0500

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,11 @@
+// This Program is written by Kashfeet Raza and Access by Code with harry
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum=6;
+    cout<<"Kashfeet Raza "<<sum;
+    return 0;
+}
