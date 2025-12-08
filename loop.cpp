32637c139a8866dea24c1e666a775c44279b01c1
Kashfeet Raza <kashfeetraza@gmail.com>
2025-12-08 21:27:45 +0500

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,32 @@
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter the value is :";
+
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<row[i];
+    }
+    
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={90,10,20,30};
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        sum = sum+arr[i];
+    }
+    cout<<"The sum of itteration is :"<<sum<<endl;
+    
+    return 0;
+}
+*/
