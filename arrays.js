a61edc9a00592ef937e9fdd5d54480fbb7a6ed62
Kashfeet Raza <kashfeetraza@gmail.com>
2025-12-10 22:32:50 +0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,128 @@
+/*let arr = [3,4,5,5,3,2]
+console.log(arr);
+
+// finding of arrays lengths
+console.log(arr.length);
+
+// index of array we can also change array if we needed to change it
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(arr[1]);
+console.log(arr[4]);
+
+// There you can see that we change the array values where we need
+arr[0] = 333
+
+console.log(arr[0]);
+
+
+
+// Typeof array convert the array into objects that we use where we need
+console.log( typeof arr);
+
+
+// If i Want to shift array into string we use tostring
+
+console.log(arr.toString());
+
+// If we want to shift the comma into anytext that we need we use join
+console.log(arr.join(" And "));
+
+// if we want to fetch last item from array we use pop
+
+console.log(arr.pop());
+
+
+// IF we want to push any thing in array
+console.log(arr.push(53));
+console.log(arr);
+
+console.log(arr.push("Kashfeet"));
+console.log(arr);
+
+// if we want to find first data we use shift where we easily access it
+
+console.log(arr.shift());
+
+// if we want to add something in begining in arrays we use unshift
+console.log(arr.unshift("kashfeet"));
+console.log(arr);
+
+
+// if we want to delete something in text we use delete
+
+delete arr[4]
+console.log(arr);
+
+*/
+
+// if we want to concatinate all type of arrays in the screen and show the data in screen
+
+// let a1 = [3,5,2,6]
+// let a2 = [4,2,1,3]
+// let a3 = [2,5,6,2]
+
+// console.log(a1.concat(a2,a3));
+
+// If we want to sorting data we can use sort
+
+// let a1 = [3,7,3,4,5,2,3,]
+
+// if we needed data to place one place to another and on the other hand we need to add an number to place there we use splice
+
+
+
+// classical for loop of Array
+
+// let arry = [5,5,3,5,3,3]
+
+// For Loop traditonal
+// for (let index = 0; index < 4; index++) {
+//     const element = arry[index];
+//     console.log(element)
+// }
+
+
+// for loop forEach
+// arry.forEach((value ,index , arry)=>{
+//     console.log(value,index,arry);
+    
+// })
+
+// for loop  forof
+
+// let obj = {
+//     a : 1,
+//     b : 2,
+//     c : 3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)){
+//         const element = obj[key]
+//         console.log(key, element);
+//     }
+    
+// }
+
+
+// forof  loop
+// let arry = [3,4,3,6,5,3]
+// for (const iterator of arry) {
+//     console.log(iterator)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
