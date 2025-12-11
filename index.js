255668bebfa294eb11eb3b7b4b9c4e8e7f283689
Kashfeet Raza <kashfeetraza@gmail.com>
2025-12-11 13:43:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// document.querySelector(".box").dataset
+// DOMStringMap {createdby: 'Ali'}
+
+//  let div = document.createElement("div")
+//         div.innerHTML = "I have boy friend" 
+//         div.setAttribute("class","alie");
+//         document.querySelector(".conta").before(div)
+
+// let cont = document.querySelector(".conta")
+// cont.insertAdjacentHTML("afterend","i can do it inshallah")
+
