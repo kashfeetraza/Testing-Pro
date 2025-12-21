691bbba4ae4db002526504a228b57d4e42cd00a6
Kashfeet Raza <kashfeetraza@gmail.com>
2025-12-21 11:44:32 +0500

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,47 +1,85 @@
-// #include<iostream>
-// using namespace std;
+//The program that are covert Fahrenheit to Celsius Degree Scaler
+//#include<iostream>
+//using namespace std;
+//
+//int convert(float a){
+//	return (a-32.0)*5.0/9.0;
+//}
+//int main(){
+//	float n;
+//	cout<<"Enter the value of Farhainte:";
+//	cin>>n;
+//	
+//	return 0;
+//cout<<"The value of Given farhainte into Celsious is:"<<convert(n);
+//}
 
 
-// int main(int argc, char const *argv[])
-// {
-//     cout<<"The value of argc is "<<argc<<endl;
+//C++ Code of simple interest
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//	float price,rate,time,interest;
+//	
+//	cout<<"Enter the price:";
+//	cin>>price;
+//	
+//	cout<<"\nEnter the Rate:";
+//	cin>>rate;
+//	
+//	cout<<"\nEnte the time:";
+//	cin>>time;
+//	
+//	interest=(price*rate*time)/100;
+//	
+//	cout<<"The interest is:"<<interest;
+//}
 
-//     for (int i = 0; i < argc; i++)
-//     {
-//         cout<<"\nThe value of argc is "<<argv[i];
-//     }
-    
-//     return 0;
-// }
+
+
+//c++ program that give us compound interest of amount
+//#include<iostream>
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//int main(){
+//	double principal=10000 , rate=5, time=2;
+//	double A = principal*((pow((1+rate/100),time)));
+//	double Compound_Interest= A - principal;
+//	
+////	cout<<"Enter the price:";
+////	cin>>principal;
+////	
+////	cout<<"\nEnter the Rate:";
+////	cin>>rate;
+////	
+////	cout<<"\nEnte the time:";
+////	cin>>time;
+//	
+//	cout<<"\nThe formula of Principle:"<<A;
+//	cout<<"\nThe Compound Interest of Principle:"<<Compound_Interest;
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int area(int a,int b){
+//	return a*b;
+//}
+//int perimeter(int a,int b){
+//	return 2*(a+b);
+//}
+//int main(){
+//	int x=9 ,y=5;
+//	
+//	cout<<"The area of rectagle is "<<area(x,y);
+//	cout<<"\nThe area of perimeter of rectagle is "<<perimeter(x,y);
+//}
+
 
 
 #include<iostream>
-using namespace std;
- 
-int main()
-{
-    int rows;
-    cout << "Enter the number of rows : ";
-    cin >> rows;
-    cout << endl;
- 
-    for (int i = 0; i < rows; i++)
-    {
-        int val = 1;
-        for (int j = 1; j < (rows - i); j++)
-        {
-            cout << "   ";
-        }
-        for (int k = 0; k <= i; k++)
-        {
-            cout << "      " << val;
-            val = val * (i - k) / (k + 1);
-        }
-        cout << endl << endl;
-    }
-    cout << endl;
-    return 0;
+
+	return 0;
 }
-
-
-
