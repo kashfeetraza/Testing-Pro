392f6b895d287178c1186061562fe204627e699a
Kashfeet Raza <kashfeetraza@gmail.com>
2025-12-22 12:30:12 +0500

    Add files via upload

---
+++ cpp_program_print_multiple_table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int s=2;
+     for (int i = 0; i <=10; i++)
+     {
+        cout<<s<<"*"<<i<<"="<<s*i<<endl;
+     }
+     
+    return 0;
+}
