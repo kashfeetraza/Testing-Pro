f0d92fd991c148f4fb2154914ab1bcd36d39098f
Kashfeet Raza <kashfeetraza@gmail.com>
2025-12-27 18:43:08 +0500

    Add files via upload

--- prog1.cpp
+++ prog1.cpp
@@ -1,7 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"Hello Chai I am Kashfeet";
+// Base class: Animal
+class Animal {
+public:
+    // Parameterized constructor for Animal
+    Animal(string type) {
+        cout << "Animal type: " << type << endl;
+    }
+};
+
+// Derived class: Dog inherits from Animal
+class Dog : public Animal {
+public:
+    // Dog constructor passes 'type' to Animal using initializer list
+    Dog(string type) : Animal(type) {
+        cout << "Dog constructor called.\n";
+    }
+};
+
+int main() {
+    // Creating Dog object calls both constructors
+    Dog d("Mammal");
+
     return 0;
 }
+
