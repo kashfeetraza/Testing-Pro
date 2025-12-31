18c59ea9887378dedb7bcee8794055f8f29f92a2
Kashfeet Raza <kashfeetraza@gmail.com>
2025-12-31 18:32:01 +0500

    Add files via upload

---
+++ program 3.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+// Include input-output stream
+using namespace std; 
+// Use standard namespace
+
+class Book { 
+// Define a class named Book
+public:
+    string title; 
+// Variable to store book title
+    string author; 
+// Variable to store book author
+
+	void display();
+
+};
+
+    void Book :: display() { 
+// Member function to print book info
+        cout << "Title: " << title << endl; // Print title
+        cout << "Author: " << author << endl; // Print author
+    }
+
+int main() {
+    Book book1; 
+// Create a Book object
+
+    book1.title = "2025";
+// Assign title
+    book1.author = "Kashfeet Raza"; 
+// Assign author
+
+    book1.display(); 
+// Call display function
+
+    return 0; 
+// Successful program termination
+}
+
