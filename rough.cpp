1c45fc515d10a07cf573961e1f95ba4483ea1c43
Kashfeet Raza <kashfeetraza@gmail.com>
2026-01-02 20:25:39 +0500

    Add files via upload

--- rough.cpp
+++ rough.cpp
@@ -1,26 +1,24 @@
 #include<iostream>
 using namespace std;
 
+class Rectangle{
+	public :
+		int length, width;
+		
+	
+		 area(){
+			cout<<"Area of rectangle:"<<length*width;
+		}
+		void input(int l,int w){
+			length = l;
+			width= w;
+		
+		}
+		
+};
+
 int main(){
-    int arr[5]={53,49,3,80,10};
-    int temp;
-
-
-    // cout << arr[0] << endl;
-    // cout << arr[1] << endl;
-    // cout << arr[2] << endl;
-    // cout << arr[3] << endl;
-    // cout << arr[4] << endl;
-
-
-    if(arr[0]<arr[1]){
-        cout << arr[0] << " " << arr[1] << " ";
-    }else{
-        temp = arr[0];
-        arr[0] = arr[1];
-        arr[1] = temp;
-
-        cout << arr[0] << " " << arr[1] << " ";
-    }
-    return 0;
+	Rectangle r;
+	r.input(4,5);
+	r.area();
 }
