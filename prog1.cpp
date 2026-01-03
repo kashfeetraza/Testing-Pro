c00d1c675dd24e262d0d88f28d3cd242b5198ba3
Kashfeet Raza <kashfeetraza@gmail.com>
2026-01-03 22:38:52 +0500

    Add files via upload

--- prog1.cpp
+++ prog1.cpp
@@ -1,28 +1,39 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  // For input/output operations
+using namespace std; // So we don't have to prefix std:: before standard functions
 
-// Base class: Animal
-class Animal {
+// Definition of the Rectangle class
+class Rectangle {
+private:
+    // Private member variables to store dimensions
+    int length, width;
+
 public:
-    // Parameterized constructor for Animal
-    Animal(string type) {
-        cout << "Animal type: " << type << endl;
+    // Public method to input values for length and width
+    void input(int l, int w) {
+        length = l;
+        width = w;
+    }
+    
+    // Declaration of a method that calculates and prints the area
+    // Takes parameters instead of using member variables directly
+    void rectangle_area(int length, int width);
+
+    // Method to compute area using stored member variables
+    void area() {
+        rectangle_area(length, width);  // Calls the member function with current values
     }
 };
 
-// Derived class: Dog inherits from Animal
-class Dog : public Animal {
-public:
-    // Dog constructor passes 'type' to Animal using initializer list
-    Dog(string type) : Animal(type) {
-        cout << "Dog constructor called.\n";
-    }
-};
+// Definition of the rectangle_area method outside the class
+void Rectangle::rectangle_area(int length, int width) {
+    // Computes and prints the area using the provided parameters
+    cout << "Area of Rectangle: " << length * width << endl;
+}
 
 int main() {
-    // Creating Dog object calls both constructors
-    Dog d("Mammal");
-
-    return 0;
+    Rectangle r;       // Create an instance of Rectangle
+    r.input(5, 3);     // Set length = 5, width = 3
+    r.area();          // Calls area() -> which calls rectangle_area()
+    return 0;          // Exit program
 }
 
