708e05bc2c625978500ea022f1b743941e10f5fa
Kashfeet Raza <kashfeetraza@gmail.com>
2026-01-15 21:02:05 +0500

    Add files via upload

---
+++ lec.js
@@ -0,0 +1,42 @@
+// constant declaration
+
+const accountId = 19923
+
+let accountEmail = "jy-ho@gmail.com"
+
+var accountPassword = "33144"
+
+acccountCity = "arifwala"
+
+/* We cannot change constant value with using const keyword we fixed the value when we use const keyword it  will be fixed the value*/
+// accountId = 34
+// console.log(accountId);
+
+
+/* Now we try to change the value of let
+now you can see the value of let can be change if we needed to change it
+
+
+accountEmail = "ali#gmoa.cp,"
+console.log(accountEmail);
+
+*/
+
+
+/* Now we try to change the value of var
+it can also change the value that we needed to change it
+
+accountPassword = "klasd"
+console.log(accountPassword);
+*/
+
+/* Now we try to change the value of var
+it can also change the value that we needed to change it
+acccountCity = "lahoer"
+console.log(acccountCity);
+*/
+
+/* USE to print all datatype to display on screen in one command
+
+console.table([accountId,accountEmail,accountPassword,acccountCity]);
+*/
