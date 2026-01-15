9d5cb26172c1522605a8bcca34bff5f0e83e85be
Kashfeet Raza <kashfeetraza@gmail.com>
2026-01-15 21:01:44 +0500

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"Use Strict"
+
+let name = "ali"
+let age = 23
+let isLogIn = true
+
+/*
+String => " "
+Integer => 323
+Bigint => 
+Boolien => True/False
+Null => we fixed the value is Null
+undefined => The value that are not assigned
+symbol => Unique
+
+object => 
+
+*/
+
+let uni
+console.log(typeof "ali",typeof 32,typeof true, typeof null, typeof uni);
+
+/* Output => string number boolean object undefined */
