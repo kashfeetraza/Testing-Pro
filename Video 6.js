c816eb08021a911ca1b797bcf32b462205f3482a
Kashfeet Raza <kashfeetraza@gmail.com>
2026-01-15 21:02:33 +0500

    Add files via upload

---
+++ Video 6.js
@@ -0,0 +1,19 @@
+
+/*
+    Conversion of data type in JavaScript
+    33 => Number
+    "33" => string
+    true => 1; False => 0
+    "33abc" => NaN
+*/
+let score = false
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let ChangeNumberToString = Number(score)
+// console.log(typeof ChangeNumberToString);
+// console.log(ChangeNumberToString);
+
+let isLogIn = 0
+let BoolenChange = Boolean(isLogIn)
+console.log(BoolenChange);
