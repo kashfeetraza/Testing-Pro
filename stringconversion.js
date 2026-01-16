4e638409e24cdb046f3e13378fe0936a8198dcc8
Kashfeet Raza <kashfeetraza@gmail.com>
2026-01-16 22:31:17 +0500

    Add files via upload

---
+++ stringconversion.js
@@ -0,0 +1,21 @@
+let value = 9
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(4 + 1);
+// console.log(4 - 1);
+// console.log(4 * 1);
+// console.log(4 / 4);
+// console.log(4 ** 3);
+// console.log(4 % 3);
+
+let str1 = "Ali"
+let str2 = " R.A.T.A"
+
+// console.log(str1 + str2);
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
