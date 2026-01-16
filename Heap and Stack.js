7b0be38d9135537e0dd11f39e85502d7e86e16ce
Kashfeet Raza <kashfeetraza@gmail.com>
2026-01-16 22:33:02 +0500

    Add files via upload

---
+++ Heap and Stack.js
@@ -0,0 +1,28 @@
+/*
+Heap (Primitive) , Stack(Primitive)
+
+*/ 
+
+
+let myFirstvariable = "ali"
+let changeVariable = myFirstvariable
+
+// console.log(myFirstvariable);
+changeVariable = "Muzammil"
+
+// console.log(myFirstvariable);
+// console.log(changeVariable);
+
+
+let student1 = {
+    rollNo: 21,
+    stuName: "Hanzi",
+    Age: 19
+}
+
+let student2 = student1
+
+student2.stuName = "rai"
+
+console.log(student1.stuName);
+console.log(student2.stuName);
